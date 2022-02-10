b78bb3f98b41f01a73850079f9c9ab48442579d5
Marcos Boggia <marcos.boggia@globant.com>
2022-02-10 17:58:34 -0300

    asd

---
+++ code2/code.py
@@ -0,0 +1,48 @@
+def sum_numbers(a, b):
+    return a + b
+
+def another_function(message):
+    print(message)
+
+    
+
+𝐍ø sum_numbersø❶_❸❾,another_functionø❹❶_❾❵
+𝐅ø❶ø❸❾þþ >>> sum_numbers(1, 2)
+    3
+    >>> sum_numbers(1, 2, 3)
+    6
+    >>> sum_numbers(1, 2, 3, 4)
+    10
+    >>> sum_numbers(1, 2, 3, 4, 5)
+    15
+    >>> sum_numbers(1, 2, 3, 4, 5, 6)
+    21
+    >>> sum_numbers(1, 2, 3, 4, 5, 6, 7)
+    42
+    >>> sum_
+𝐅ø❹❶ø❾❵þþ This is a simple example of a function that takes a string and returns a string
+representing the string with all the characters in uppercase.
+
+The function is written in Python 2.7.
+
+The function is written in a way that it can be used as a decorator.
+
+The decorator is used to make the function more readable.
+
+The decorator is used to make the function more readable.
+
+The decorator is used to make the function more readable.
+
+The decorator is used to make the function more readable.
+
+The decorator is used to make the function more readable.
+
+The decorator is used to make the function more readable.
+
+The decor
+𝐓ø❶ø❸❾þþ addition, numbers
+𝐓ø❹❶ø❾❵þþ function, message
+𝐋ø❷ø❶ø❷❸þþ Sum of numbers
+𝐋ø❸ø❷❸ø❸❾þþ Return the sum of a and b }
+𝐋ø❺ø❹❶ø❼❷þþ Define a function called 'another_function' that takes
+𝐋ø❻ø❼❷ø❾❵þþ Prints message
