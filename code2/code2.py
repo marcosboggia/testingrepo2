58445d155de8d8b4c4137f91fb957349efbc47d5
Marcos Boggia <marcos_boggia@hotmail.com>
2022-02-11 09:32:15 -0300

    Update code2.py

--- code2/code2.py
+++ code2/code2.py
@@ -8,38 +8,3 @@ def multiply(numbers):
     return a
 
     
-
-𝐍ø helloworldø❶_❹❷,multiplyø❹❹_❶❸❷
-𝐅ø❶ø❹❷þþ This is a docstring for the helloworld function.
-    
-    This is a docstring for the helloworld function.
-    
-    This is a docstring for the helloworld function.
-    
-    This is a docstring for the helloworld function.
-    
-    This is a docstring for the helloworld function.
-    
-    This is a docstring for the helloworld function.
-    
-    This is a docstring for the helloworld function.
-𝐅ø❹❹ø❶❸❷þþ This module provides a simple function to multiply a list of numbers.
-
-The function takes a list of numbers as an argument and returns a list of
-the same length as the input list, with each element of the list
-multiplied by the corresponding element of the input list.
-
->>> multiply( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] )
-[1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 362880]
-
->>> multiply( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] )
-[1, 2, 6, 24,
-𝐓ø❶ø❹❷þþ hello, world
-𝐓ø❹❹ø❶❸❷þþ numbers, add, multiplication
-𝐋ø❷ø❶ø❶❽þþ Method ef helloworld()
-𝐋ø❸ø❶❽ø❹❷þþ Return a string
-𝐋ø❺ø❹❹ø❻❼þþ Multiply numbers
-𝐋ø❻ø❻❼ø❼❼þþ Set the variable to zero
-𝐋ø❼ø❼❼ø❶❵❷þþ ForEach loop
-𝐋ø❽ø❶❵❷ø❶❷❵þþ Increment the value of a by one.
-𝐋ø❾ø❶❷❵ø❶❸❷þþ Return value of a
